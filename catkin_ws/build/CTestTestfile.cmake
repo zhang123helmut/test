f8b2cc5e1f7662e683e8e76e14823a38dd183646
Ananke2021 <1057542559@qq.com>
2021-07-03 11:07:52 +0200

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/annake/test/catkin_ws/src
+# Build directory: /home/annake/test/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arbotix_ros/arbotix")
+subdirs("arbotix_ros/arbotix_controllers")
+subdirs("arbotix_ros/arbotix_firmware")
+subdirs("arbotix_ros/arbotix_python")
+subdirs("arbotix_ros/arbotix_sensors")
+subdirs("arbotix_ros/arbotix_msgs")
+subdirs("mbot_description")
