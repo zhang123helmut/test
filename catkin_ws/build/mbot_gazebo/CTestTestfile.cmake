cbff1abc8263e649152955bfbbe9d02beb22195b
Ananke2021 <1057542559@qq.com>
2021-07-04 15:15:48 +0200

    2 korrigieren

---
+++ catkin_ws/build/mbot_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/annake/test/catkin_ws/src/mbot_gazebo
+# Build directory: /home/annake/test/catkin_ws/build/mbot_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
